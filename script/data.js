380c39e6d23604dc17e753890f2f9294f4b10202
tclh123 <tclh123@gmail.com>
2013-04-15 01:38:38 +0800

    add some initial htm\js\css

---
+++ script/data.js
@@ -0,0 +1,23 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: tclh123
+ * Date: 13-4-15
+ * Time: 上午12:52
+ */
+
+
+var testdata = [
+    {
+        floor : 1,
+//        color : ,
+        position : { x:1, z:1 }
+    },
+    {
+        floor : 1,
+        position : { x:2, z:1 }
+    }
+];
+
+INDO.Data = function() {
+
+};
---
+++ script/indo.js
@@ -0,0 +1,8 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: tclh123
+ * Date: 13-4-15
+ * Time: 上午12:54
+ */
+
+var INDO = INDO || { version : '1.0' };
